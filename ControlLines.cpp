2a4e0b7fbb752740d1f479a827f5b4244f76c2fa
Paul Harvey <Paul Harvey>
2022-10-19 21:55:00 +0100

    Updated comments

--- ControlLines.cpp
+++ ControlLines.cpp
@@ -13,6 +13,7 @@ ControlLines::ControlLines()
 void ControlLines::set(unsigned int lines)
 {
     digitalWrite(SHIFT_LATCH_PIN, LOW);
+
     shiftOut(SHIFT_DATA_PIN, SHIFT_CLK_PIN, MSBFIRST, (byte)lines);
     shiftOut(SHIFT_DATA_PIN, SHIFT_CLK_PIN, MSBFIRST, (byte)(lines >> 8));
 
